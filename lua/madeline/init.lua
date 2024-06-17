ca6987149d3a301e3773865dff49c115a5fdee09
madeline-dw <oficialclankyrblx@gmail.com>
2024-06-17 21:27:04 +0000

    change to lazy.nvim

--- lua/madeline/init.lua
+++ lua/madeline/init.lua
@@ -1,6 +1,6 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-require("theprimeagen.lazy_init")
+require("madeline.set")
+require("madeline.remap")
+require("madeline.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
--- lua/madeline/lazy/snippets.lua
+++ lua/madeline/lazy/snippets.lua
@@ -2,6 +2,8 @@
 return {
     {
         "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/nvim-cmp",
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!).
