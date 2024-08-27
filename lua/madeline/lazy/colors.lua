62792bb05b89a14a19cb7e3f6b398b8c19b80be3
madeline-dw <oficialclankyrblx@gmail.com>
2024-08-26 21:49:21 -0600

    from laptop1

--- lua/madeline/lazy/colors.lua
+++ lua/madeline/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -87,7 +87,39 @@ return {
         end
     },
     {
-        "ellisonleao/gruvbox.nvim"
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = false, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = false,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "soft", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {
+
+                },
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+
+            vim.cmd("colorscheme gruvbox")
+
+            ColorMyPencils("gruvbox")
+        end
     },
 
 }
